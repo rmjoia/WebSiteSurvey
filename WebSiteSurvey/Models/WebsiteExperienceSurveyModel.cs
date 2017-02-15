69e44530b784365fb56c9397fe50be7b5b9926a8
Ricardo Melo Joia <ricardo.joia@live.com.pt>
2017-02-15 18:56:40 +0000

    added WebsiteExperienceSurveyModel

---
+++ WebSiteSurvey/Models/WebsiteExperienceSurveyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSiteSurvey.Models
+{
+    public class WebsiteExperienceSurveyModel
+    {
+        public WebsiteExperienceSurveyModel(string referrer)
+        {
+            Referrer = referrer;
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public short ExperienceRating { get; set; }
+        public string Suggestion { get; set; }
+        public string Referrer { get; }
+    }
+}
