942c51d6f7fa22de8deb7c6b648f06483841abc2
Ricardo Melo Joia <ricardo.joia@live.com.pt>
2017-02-15 20:35:23 +0000

    added survey form

--- WebSiteSurvey/Controllers/HomeController.cs
+++ WebSiteSurvey/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using WebSiteSurvey.Models;
 
 namespace WebSiteSurvey.Controllers
 {
@@ -10,7 +12,44 @@ namespace WebSiteSurvey.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var referrer = GetReferrer();
+
+            var newSurvey = new WebsiteExperienceSurveyModel(referrer);
+            return View(newSurvey);
+        }
+
+        public ActionResult Submit(WebsiteExperienceSurveyModel websiteExperienceSurveyModel)
+        {
+            return Redirect("http://www.disney.com");
+        }
+
+        public ActionResult Clear(WebsiteExperienceSurveyModel websiteExperienceSurveyModel)
+        {
+            websiteExperienceSurveyModel = new WebsiteExperienceSurveyModel(websiteExperienceSurveyModel.Referrer);
+            return View("Index", websiteExperienceSurveyModel);
+        }
+
+        public ActionResult SubmitForm(string action)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        private string GetReferrer()
+        {
+            var referrer = string.Empty;
+
+            if (Session["referrer"] != null) return referrer;
+
+            referrer = Request.UrlReferrer?.ToString() ?? "unkown";
+            Session.Add("referrer", referrer);
+            return referrer;
+        }
+
+        public ActionResult Edit(WebsiteExperienceSurveyModel model)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- WebSiteSurvey/Models/WebsiteExperienceSurveyModel.cs
+++ WebSiteSurvey/Models/WebsiteExperienceSurveyModel.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace WebSiteSurvey.Models
 {
@@ -17,8 +21,33 @@ namespace WebSiteSurvey.Models
         public int Age { get; set; }
         public string Gender { get; set; }
         public string Country { get; set; }
+
+        [Display(Name = "Rate your experience")]
         public short ExperienceRating { get; set; }
+
         public string Suggestion { get; set; }
         public string Referrer { get; }
+
+        public SelectList Genders { get; set; } = new SelectList(GenderList(), "Value", "Text");
+
+        public SelectList Countries = new SelectList(GetCountries(), "Text", "Value");
+
+        private static IEnumerable<SelectListItem> GenderList()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem() {Value = "Male", Text = "Male"},
+                new SelectListItem() {Value = "Female", Text = "Female"}
+            };
+        }
+
+        private static IEnumerable<SelectListItem> GetCountries()
+        {
+            return CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                .Select(c => new RegionInfo(c.LCID).EnglishName)
+                .Distinct()
+                .OrderBy(c => c)
+                .Select(c => new SelectListItem {Text = c, Value = c});
+        }
     }
 }
